e1702178d68fae00eeb1c8ba1ccbe27a0544d591
franciscorichter <franciscorichter@gmail.com>
2023-07-24 13:55:23 +0200

    Update load_NN.R

--- R/load_NN.R
+++ R/load_NN.R
@@ -1,4 +1,9 @@
 .onLoad <- function(libname, pkgname) {
+  if (!"torch" %in% installed.packages()) {
+    message("The 'torch' package is not installed. Installing now...")
+    install.packages("torch")
+    torch::install_torch()
+  }
   model_path <- system.file("data", "NNtest.pt", package = "NNemesis")
   assign("cnn_ltt", torch::torch_load(model_path), envir = .GlobalEnv)
 }
